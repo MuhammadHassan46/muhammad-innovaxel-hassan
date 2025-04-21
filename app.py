77d9a6d4d4e68f8d4f7842bba12577edcae1ef01
MuhammadHassan46 <muhammadhassanzahid4@gmail.com>
2025-04-21 14:16:01 +0500

    Add redirect route for short codes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, abort
 from models import db, URL
 from config import Config
 
@@ -13,6 +13,15 @@ with app.app_context():
 def index():
     urls = URL.query.all()
     return render_template('index.html', urls=urls)
+
+@app.route('/<short_code>')
+def redirect_to_url(short_code):
+    url_data = URL.query.filter_by(short_code=short_code).first()
+    if url_data:
+        url_data.access_count += 1
+        db.session.commit()
+        return redirect(url_data.original_url)
+    return abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
